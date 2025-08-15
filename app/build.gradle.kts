6575385c5ce110ecb764a7f2c214691c0f123d2d
Pranav-24006 <pranavverma006@gmail.com>
2025-08-15 14:06:47 +0530

    Modified event screen and the planner screen, and removed unused imports, and modified the ui of the events screen as well as the planner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
     implementation ("io.coil-kt:coil-compose:2.4.0")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
